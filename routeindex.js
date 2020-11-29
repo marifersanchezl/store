6bbaccd0a4ca37ba79bbd386c12554d82552acf8
Raul Herrera <raul_ehs@hotmail.com>
2020-11-29 15:44:39 -0600

    Display homepage products using EJS and JS array with data

--- routeindex.js
+++ routeindex.js
@@ -2,8 +2,51 @@ const { render } = require('ejs');
 const express = require('express');
 const router = express.Router();
 
+productsHome = [
+  {
+    name: "Panditas Enchilados",
+    desc: "panditas",
+    img: "Panditas-ANIMATION.gif"
+  },
+  {
+    name: "Gusanitos Enchilados",
+    desc: "gusanos",
+    img: "gusanos.jpg"
+  },
+  {
+    name: "Cookie Bites",
+    desc: "cookies",
+    img: "cookies-ANIMATION.gif"
+  },
+  {
+    name: "Brownies",
+    desc: "brownies",
+    img: "brownie.jpg"
+  },
+  {
+    name: "Aros de Durazno Enchilados",
+    desc: "durazno",
+    img: "arosD-ANIMATION.gif"
+  },
+  {
+    name: "Aros de Manzana Enchilados",
+    desc: "manzana",
+    img: "arosM-ANIMATION.gif"
+  },
+  {
+    name: "Arandanos Enchilados",
+    desc: "arandanos",
+    img: "Arandanos.jpg"
+  },
+  {
+    name: "Mix de Gomitas Enchiladas",
+    desc: "gomitas",
+    img: "IMG_3686-ANIMATION.gif"
+  },
+];
+
 router.get("/", function(req, res) {
-  res.render('home');
+  res.render('home', {productsHome});
 });
 
 router.get("/login", function(req, res) {
