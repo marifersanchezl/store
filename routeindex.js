6832617758de035357f34567fd38b6bd04f83d4a
Raul Herrera <raul_ehs@hotmail.com>
2020-11-29 18:51:19 -0600

    Display products page using EJS and JS array with data

--- routeindex.js
+++ routeindex.js
@@ -2,51 +2,189 @@ const { render } = require('ejs');
 const express = require('express');
 const router = express.Router();
 
-productsHome = [
+products = [
   {
     name: "Panditas Enchilados",
     desc: "panditas",
-    img: "Panditas-ANIMATION.gif"
+    prices: [
+      {
+        size: "Pouch 250g",
+        price: "$50"
+      },
+      {
+        size: "Pouch 450g",
+        price: "$80"
+      },
+      {
+        size: "Jar 240g",
+        price: "$60"
+      },
+      {
+        size: "Jar 460g",
+        price: "$100"
+      }
+    ],
+    imgHome: "Panditas-ANIMATION.gif",
+    img: "Panditas.jpg"
   },
   {
     name: "Gusanitos Enchilados",
     desc: "gusanos",
-    img: "gusanos.jpg"
+    prices: [
+      {
+        size: "Pouch 250g",
+        price: "$50"
+      },
+      {
+        size: "Pouch 450g",
+        price: "$80"
+      },
+      {
+        size: "Jar 240g",
+        price: "$60"
+      },
+      {
+        size: "Jar 460g",
+        price: "$100"
+      }
+    ],
+    imgHome: "gusanos.jpg",
+    img: "gusanosTop.jpg"
   },
   {
     name: "Cookie Bites",
     desc: "cookies",
-    img: "cookies-ANIMATION.gif"
+    prices: [
+      {
+        size: "Pouch chico",
+        price: "$80"
+      },
+      {
+        size: "Pouch grande",
+        price: "$150"
+      },
+      {
+        size: "Jar chico",
+        price: "$60"
+      },
+      {
+        size: "Jar grande",
+        price: "$100"
+      }
+    ],
+    imgHome: "cookies-ANIMATION.gif",
+    img: "cookies.jpg"
   },
   {
     name: "Brownies",
     desc: "brownies",
+    prices: [
+      {
+        size: "Pouch chico",
+        price: "$80"
+      },
+      {
+        size: "Pouch grande",
+        price: "$150"
+      },
+      {
+        size: "Jar grande",
+        price: "$100"
+      }
+    ],
+    imgHome: "brownie.jpg",
     img: "brownie.jpg"
   },
   {
     name: "Aros de Durazno Enchilados",
     desc: "durazno",
-    img: "arosD-ANIMATION.gif"
+    prices: [
+      {
+        size: "Pouch 250g",
+        price: "$50"
+      },
+      {
+        size: "Pouch 450g",
+        price: "$80"
+      },
+      {
+        size: "Jar 240g",
+        price: "$60"
+      },
+      {
+        size: "Jar 460g",
+        price: "$100"
+      }
+    ],
+    imgHome: "arosD-ANIMATION.gif",
+    img: "arosD.jpg"
   },
   {
     name: "Aros de Manzana Enchilados",
     desc: "manzana",
-    img: "arosM-ANIMATION.gif"
+    prices: [
+      {
+        size: "Pouch 250g",
+        price: "$50"
+      },
+      {
+        size: "Pouch 450g",
+        price: "$80"
+      },
+      {
+        size: "Jar 240g",
+        price: "$60"
+      },
+      {
+        size: "Jar 460g",
+        price: "$100"
+      }
+    ],
+    imgHome: "arosM-ANIMATION.gif",
+    img: "arosM.jpg"
   },
   {
     name: "Arandanos Enchilados",
     desc: "arandanos",
+    prices: [
+      {
+        size: "Pouch 250g",
+        price: "$70"
+      },
+      {
+        size: "Pouch 450g",
+        price: "$100"
+      },
+      {
+        size: "Jar 240g",
+        price: "$80"
+      },
+      {
+        size: "Jar 460g",
+        price: "$120"
+      }
+    ],
+    imgHome: "Arandanos.jpg",
     img: "Arandanos.jpg"
   },
   {
     name: "Mix de Gomitas Enchiladas",
     desc: "gomitas",
-    img: "IMG_3686-ANIMATION.gif"
+    prices: [],
+    imgHome: "IMG_3686-ANIMATION.gif",
+    img: "IMG_3686-ANIMATION.gif",
+  },
+  {
+    name: "Nuez garapiñada",
+    desc: "nuez",
+    prices: [],
+    imgHome: "nuez.jpg",
+    img: "nuez.jpg"
   },
 ];
 
 router.get("/", function(req, res) {
-  res.render('home', {productsHome});
+  res.render('home', {products});
 });
 
 router.get("/login", function(req, res) {
@@ -62,7 +200,7 @@ router.get("/product", function(req, res) {
 });
 
 router.get("/productsAll", function(req, res) {
-  res.render('productsAll');
+  res.render('productsAll', {products});
 });
 
 router.get("/checkout", function(req, res) {
